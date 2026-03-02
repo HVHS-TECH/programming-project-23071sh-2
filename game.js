51c79eaf3bad50eb2be793aebe908906ab37aaa0
23071sh <23071sh@hvhs.school.nz>
2026-03-03 12:27:34 +1300

    3/3

---
+++ game.js
@@ -0,0 +1,22 @@
+/*******************************************************/
+// P5.play: Space runner game 
+// PLay screen 
+/// Written by Sonia Hassan
+/*******************************************************/
+
+//setup()
+function setup() {
+console.log("setup: ");
+cnv = new Canvas(800, 700);
+
+
+astronaut = new Sprite(200, 300, 50, 100, 'd');
+astronaut.color = 'purple';
+box.vel.x = 2;
+}
+
+//Draw()
+function draw() {
+background('grey');		
+}
+
