9d0ed821dc3e9152364425dc62e30930c8c688f4
23071sh <23071sh@hvhs.school.nz>
2026-03-04 15:13:58 +1300

    4/3

--- game.js
+++ game.js
@@ -3,13 +3,22 @@
 // PLay screen 
 /// Written by Sonia Hassan
 /*******************************************************/
-
+function preload() {
+  imgBG   = loadImage('imgs/sky.jpeg');
+}
 
 function setup(){
 console.log("setup:");
-createCanvas(windowWidth - PADDING, windowHeight - PADDING);
+createCanvas(windowWidth, windowHeight);
+
+
+//Astronout 
+box = new Sprite(200, 300, 50, 100, '20');
+box.color = 'white';
+
 }
 
-function draw(){
-background('grey')
+
+function draw(){ 
+    background(imgBG);
 }
