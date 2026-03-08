10d4027d44e6bb457bb42fdf64d7090ac1703605
23071sh <23071sh@hvhs.school.nz>
2026-03-09 11:02:03 +1300

    9/3

--- game.js
+++ game.js
@@ -4,17 +4,24 @@
 /// Written by Sonia Hassan
 /*******************************************************/
 function preload() {
-  imgBG   = loadImage('imgs/sky.jpeg');
+imgBG   = loadImage('imgs/sky.jpeg');
+
+astronout_start = loadAnimation('imgs/startposition.png');
 }
 
 function setup(){
 console.log("setup:");
 createCanvas(windowWidth, windowHeight);
 
-
 //Astronout 
-box = new Sprite(200, 300, 50, 100, '20');
-box.color = 'white';
+astronout = new Sprite(200, 300, 50, 100, '20');
+astronout.addAnimation("start position", astronout_start );
+astronout.scale = 0.15;
+
+//Obstacles
+
+
+
 
 }
 
