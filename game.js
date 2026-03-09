039725c53a8178ea8f3b401dc3095a5ce48e9894
23071sh <23071sh@hvhs.school.nz>
2026-03-10 12:27:46 +1300

    10/3

--- game.js
+++ game.js
@@ -14,16 +14,23 @@ console.log("setup:");
 createCanvas(windowWidth, windowHeight);
 
 //Astronout 
-astronout = new Sprite(200, 300, 50, 100, '20');
+astronout = new Sprite(50, 800, 20, 50, 'k');
 astronout.addAnimation("start position", astronout_start );
-astronout.scale = 0.15;
+astronout.scale = 0.2;
 
 //Obstacles
+obstaclesGroup = new Group();
+object1 = new Sprite(400, 800, 80, 90, 'k')
+object2 = new Sprite(600, 800, 80, 90, 'k')
+object3 = new Sprite(800, 800, 80, 90, 'k')
+object4 = new Sprite(1000, 800, 80, 90, 'k')
 
-
-
-
+//Ground 
+ground = new Sprite(200, 810, 3440, 20,);
+ground.color = 'white';
 }
+
+
 
 
 function draw(){ 
