481d8c98dc6494fccf28c04d42e97cf9c94369de
23071sh <23071sh@hvhs.school.nz>
2026-03-17 18:31:14 +1300

    saveeee

--- game.js
+++ game.js
@@ -1,71 +1,68 @@
 /*******************************************************/
 // P5.play: Space runner game 
-// PLay screen 
-/// Written by Sonia Hassan
+// Play screen 
+// Written by Sonia Hassan
 /*******************************************************/
-let PLAY = 1;
-let END = 0;
+let PLAY = 1, END = 0;
 let gameState = PLAY;
 
-let ground, invisibleGround, groundImage;
+let ground, groundImage;
+let astronaut, astronaut_start;
+let imgBG;
+let obstacle1Sprite, obstacle2Sprite, obstacle3Sprite;
+let obstacle1, obstacle2, obstacle3;
+let score = 0;
+let gravity = 1;
 
 function preload() {
-//Imaes/animations 
-imgBG = loadImage('imgs/sky.jpeg');
-
-groundImage = loadAnimation('imgs/ground.png');
-
-astronout_start = loadAnimation('imgs/startposition.png');
-
-obstacle1 = loadAnimation('imgs/neptune.png');
-obstacle2 = loadAnimation('imgs/earth.png');
-obstacle3 = loadAnimation('imgs/planet.png'); 
+  imgBG = loadImage('imgs/sky.jpeg');
+  groundImage = loadAnimation('imgs/ground.png');
+  astronaut_start = loadAnimation('imgs/startposition.png');
+  obstacle1 = loadAnimation('imgs/neptune.png');
+  obstacle2 = loadAnimation('imgs/earth.png');
+  obstacle3 = loadAnimation('imgs/planet.png');
 }
 
-function setup(){
-console.log("setup:");
-createCanvas(windowWidth, windowHeight);
+function setup() {
+  createCanvas(windowWidth, windowHeight);
 
-//Astronout 
-astronout = new Sprite(50, 850, 20, 50, 'k');
-astronout.addAnimation("start position", astronout_start);
-astronout.scale = 0.3;
+  // Astronaut
+  astronaut = new Sprite(100, height - 80, 50, 50);
+  astronaut.addAnimation("start", astronaut_start);
+  astronaut.scale = 0.3;
 
-//Ground
-ground = new Sprite(width/2, height-20);
-ground.addAnimation(groundImage);
-ground.scale = 2;
-ground.velocity.x = -6;
+  // Invisible ground
+  let invisibleGround = new Sprite(width / 2, height - 20, width, 40);
+  invisibleGround.visible = false;
+  astronaut.collide(invisibleGround);
 
-// Invisible ground (so astronaut doesn't fall)
-invisibleGround = new Sprite(width/2, height-20, width*2, 40);
-invisibleGround.visible = false;
+  // Ground: two sprites for looping
+  ground = new Sprite(width / 6, height - 30);
+  ground.addAnimation(groundImage);
+  ground.scale = 3;
+  ground.velocity.x = -8;
 
-// Obstacles
-obstacle1Sprite = new Sprite(width+200, height-80, 50, 50, 'k');
-obstacle1Sprite.addAnimation("neptune", obstacle1);
-obstacle1Sprite.scale = 0.2
-obstacle1Sprite.velocity.x = -6; // obstacle moves left
 
-obstacle2Sprite = new Sprite(width+600, height-80, 50, 50, 'k');
-obstacle2Sprite.addAnimation("earth", obstacle2);
-obstacle2Sprite.scale = 0.2
-obstacle2Sprite.velocity.x = -6; // obstacle moves left
+  // Obstacles
+  obstacle1Sprite = new Sprite(width + 100, height - 80, 50, 50);
+  obstacle1Sprite.addAnimation("neptune", obstacle1);
+  obstacle1Sprite.scale = 0.2;
+  obstacle1Sprite.velocity.x = -6;
 
-obstacle3Sprite = new Sprite(width+1000, height-80, 50, 50, 'k');
-obstacle3Sprite.addAnimation("planet", obstacle3);
-obstacle3Sprite.scale = 0.2
-obstacle3Sprite.velocity.x = -6; // obstacle moves left
+  obstacle2Sprite = new Sprite(width + 400, height - 80, 50, 50);
+  obstacle2Sprite.addAnimation("earth", obstacle2);
+  obstacle2Sprite.scale = 0.2;
+  obstacle2Sprite.velocity.x = -6;
+
+  obstacle3Sprite = new Sprite(width + 700, height - 80, 50, 50);
+  obstacle3Sprite.addAnimation("planet", obstacle3);
+  obstacle3Sprite.scale = 0.22;
+  obstacle3Sprite.velocity.x = -6;
 }
 
-function draw(){ 
-background(imgBG);
+function draw() {
+  background(imgBG);
 
-// Move ground forever
- if(gameState === PLAY){
- // Ground movement
-if (ground.position.x < width/6 - ground.width/6){
- ground.position.x = width/3;
-}
- }
+  if (gameState === PLAY) {
+  }
 }
